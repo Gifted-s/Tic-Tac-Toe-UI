b55adda219cb171eb90c34244298d01241161ec9
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-04-16 20:13:52 +0100

    made adjustmnent

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,4 @@
 export default {
   API_ROOT: 'https://tic-tac-toe-socket-server1.herokuapp.com',
-  // API_ROOT: 'http://192.168.73.79:5000',
   FRONTEND_ROOT: 'https://tic-tac-toe01.netlify.app'
-  // FRONTEND_ROOT: 'http://192.168.73.79:3000'
 };
